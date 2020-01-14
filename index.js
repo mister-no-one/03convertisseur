303862b9c37a5b9a0e6f4e31adc845c2161b051c
Greg <greg@mail.com>
2020-01-14 17:08:15 +0100

    Update rate button added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 	var rate = 1.11;
 
 	function updateRate(){
+		console.log("Debut requete")
 		var request = new XMLHttpRequest();
 		request.open("GET","https://api.exchangeratesapi.io/latest");
 		request.send();
@@ -9,10 +10,13 @@
 			var dataText = event.target.responseText;
 			var data = JSON.parse(dataText);
 			rate = data.rates.USD;
+
+			console.log(rate);
 		});
 	}
 
 	function processConvert(){
+		
 		// RECUPERATION VALEURS
 		var valueElement = document.querySelector(".userInput input[name=inputValue]");
 		var value = valueElement.value;
@@ -31,54 +35,18 @@
 		toValueElement.innerHTML = result + '$';
 	}
 
+	// UPDATE RATE
 	updateRate();
 	var element = document.querySelector(".userInput");
 	element.classList.remove("disabled");
 
-
+	// PROCESS CONVERT
 	var formElement = document.querySelector(".userInput form");
 	formElement.addEventListener("submit",function(event){
 		event.preventDefault();
 		processConvert();
 	})
 
-
-
-
-
-// var rate = 1.11;
-
-// var request = new XMLHttpRequest();
-// request.open("GET","https://api.exchangeratesapi.io/latest");
-// request.send();
-
-// request.addEventListener("load",function(event){
-
-// 	var dataText = event.target.responseText;
-// 	var data = JSON.parse(dataText);
-
-// 	rate = data.rates.USD;
-
-// 	var element = document.querySelector(".userInput");
-// 	element.classList.remove("disabled");
-// });
-
-
-// var formElement = document.querySelector(".userInput form");
-// formElement.addEventListener("submit",function(event){
-// 	event.preventDefault();
-// 	var valueElement = document.querySelector(".userInput input[name=inputValue]");
-// 	var value = valueElement.value;
-
-// 	if(!value){
-// 		value = 1;
-// 	}
-
-// 	var result = value * rate;
-
-// 	var fromValueElement = document.querySelector(".result .fromValue");
-// 	var toValueElement = document.querySelector(".result .toValue");
-
-// 	fromValueElement.innerHTML = value + '€';
-// 	toValueElement.innerHTML = result + '$';
-// })
+	// REFRESH RATE ON CLIC
+	var buttonRefresh = document.querySelector(".btn-refresh");
+	buttonRefresh.addEventListener("click",updateRate);
