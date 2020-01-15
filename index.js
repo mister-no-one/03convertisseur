e4e301df37ed49d86c95c94ad5a525d74642e248
Greg <greg@mail.com>
2020-01-15 15:28:16 +0100

    Button USD and PHP added

--- index.js
+++ index.js
@@ -6,11 +6,32 @@
 	updateRate();
 
 	var btnYEN = document.querySelector(".currency-jpy");
+	var btnUSD = document.querySelector(".currency-usd");
+	var btnPHP = document.querySelector(".currency-php");
+
 	btnYEN.addEventListener("click",function(event){
 		currencyJPY = event.target.getAttribute("data-currency");
 		symbolJPY = event.target.getAttribute("data-symbol");
 		currentCurrency = currencyJPY;
 		symbol = symbolJPY;
+		rate = rates[currentCurrency];
+		processConvert();
+	})
+
+	btnUSD.addEventListener("click",function(event){
+		currencyUSD = event.target.getAttribute("data-currency");
+		symbolUSD = event.target.getAttribute("data-symbol");
+		currentCurrency = currencyUSD;
+		symbol = symbolUSD;
+		rate = rates[currentCurrency];
+		processConvert();
+	})
+
+	btnPHP.addEventListener("click",function(event){
+		currencyPHP = event.target.getAttribute("data-currency");
+		symbolPHP = event.target.getAttribute("data-symbol");
+		currentCurrency = currencyPHP;
+		symbol = symbolPHP;
 		rate = rates[currentCurrency];
 		processConvert();
 	})
